c3ca11a1f603718bb233e74da3ace8c8a39d9b50
Dat Adithya <naruita201@gmail.com>
2020-05-02 00:18:16 +0530

    GOT THE CODE WORKING

--- simpleSchedule/test_class_timetablemaker.py
+++ simpleSchedule/test_class_timetablemaker.py
@@ -1,3 +1,5 @@
+import pickle
+
 class day_timetable:
     def __init__(self, day):
         self.day = day
@@ -15,15 +17,30 @@ class day_timetable:
             else:
                 self.links.append(temp_link)
         self.time_table[self.in_time] = {self.course_code: self.links}
+        print(str(self.time_table))
     
     def day_return(self):
         return self.day
 
     def put_info(self):
+        print(str(self.time_table))
         return self.time_table
 
 # available_days = r'^[sun,mon,tues,wednes,thurs,fri]day$' need to verify days later
+week_timetable = {}
 
-input_day_name = input("Enter the day : ").lower()
-day_timetable(input_day_name).get_info()
-day_timetable()
+while True:
+    input_day_name = input("Enter the day : ").lower()
+    current_day = day_timetable(input_day_name)
+    while True:
+        if input('Enter the upcoming class?(y/n)') == 'y':
+            current_day.get_info()
+            week_timetable[current_day.day_return()] = current_day.put_info()
+            print(week_timetable)
+        else:
+            break
+    if input("Do you wish to continue?(y/n) : ") == 'n':
+        break
+
+with open('schedule.txt', 'w') as f:
+    f.write(str(week_timetable))
