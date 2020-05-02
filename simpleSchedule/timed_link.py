7ba40380bb29ae753def9a64583bed286a7f1a6a
54742586 <vijaybalajitheboss@gmail.com>
2020-05-02 14:29:35 +0530

    Added code to launch application from link

--- simpleSchedule/timed_link.py
+++ simpleSchedule/timed_link.py
@@ -1,12 +1,19 @@
 import datetime
 import webbrowser as webb
 from time import sleep
+from pynput.keyboard import Key, Controller
+import time
 
+keyboard = Controller()
 op = webb.get('opera')
 
 def link_open():
     op.open("https://m.teamlink.co/7706072174?p=83b0953304ad70075faa84c94f63a6fd")
-
+    time.delay(20)
+    keyboard.press(Key.left)
+    keyboard.release(Key.left)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 now = datetime.datetime.now()
 print(now)
 run_at = now + datetime.timedelta(seconds=5)
