afaa7abe834f7dcfac94e732323cbf16f9651cc1
Dat Adithya <naruita201@gmail.com>
2020-05-04 15:59:14 +0530

    Added the GUI but, due to #10 and #11 again.

--- simpleSchedule/timetable_GUI.py
+++ simpleSchedule/timetable_GUI.py
@@ -1,8 +1,16 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Button, LEFT, RIGHT, TOP, BOTTOM, X, Menu, Label, SUNKEN, W
 from timetablemaker import make
 from timed_link import execute
 
 root = Tk()
 root.title("It's the Time Tabler.")
+
+maker = Button(root, text="Make time table", command=make)
+maker.pack(side=LEFT)
+execute = Button(root, text="Run", command=execute)
+execute.pack(side=RIGHT)
+
+status_bar = Label(root, text="Status bar", bd=1, relief=SUNKEN, anchor=W)
+status_bar.pack(side=BOTTOM, padx=2, fill=X)
 
 root.mainloop()
