103ebc1cb66d0cb1f31c15aefd678681537fd1fc
Dat Adithya <naruita201@gmail.com>
2020-05-04 18:16:44 +0530

    added in a bit of sleep since the apps cannot open

--- simpleSchedule/deploy.py
+++ simpleSchedule/deploy.py
@@ -1,7 +1,7 @@
 import os
 import re
 import webbrowser as webb
-import time
+from time import sleep
 from pynput.keyboard import Key, Controller
 import pickle
 
@@ -10,17 +10,19 @@ def deploy_links(team_links):
     for team_link in team_links:
         if re.match(r"^https://m.teamlink.co", team_link):
             webb.open(team_link)
-            time.sleep(20)
+            sleep(20)
             keyboard.press(Key.left)
             keyboard.release(Key.left)
             keyboard.press(Key.enter)
             keyboard.release(Key.enter)
+            sleep(6)
         elif re.match(r"^https://us02web.zoom.us", team_link):
             webb.open(team_link)
-            time.sleep(20)
+            sleep(20)
             keyboard.press(Key.left)
             keyboard.release(Key.left)
             keyboard.press(Key.enter)
             keyboard.release(Key.enter)
+            sleep(6)
         else:
             webb.open(team_link)
